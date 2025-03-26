5f21d1072cc77f999da3be2072bfc04e7da10b43
kareenap55 <kareenap1412@gmail.com>
2025-03-26 10:12:30 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+ window.sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+function playSound(sound) {
+  stopSounds();
+  const audio = new Audio(./sounds/${sound}.mp3);
+  
+  audio.onerror = () => console.error(Error loading sound: ${sound}.mp3);
+  
+  audio.play();
+  audio.id = sound;
+  document.body.appendChild(audio);
+}
+
+function stopSounds() {
+  window.sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+      audio.remove();
+    }
+  });
+}
+
